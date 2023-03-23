a8c18ce5ff779b48b9226018ad4782441323fac2
PutriHabibahDevina <putrihdm13@gmail.com>
2023-03-23 19:31:33 +0700

    Membuat jarak per baris dan membuat tampilan susunan data elemen array

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -20,4 +20,8 @@ void input()
 			cout << "\nArray yang anda masukkan maksimal 20 elemen, \n";	// Menampilkan pesan jika data lebih dari 20
 		}
 	}
+	cout << endl;								// Membuat jarak per baris program
+	cout << "====================" << endl;		// Membuat tampilan susunan data element array
+	cout << "Masukkan Element Array" << endl;
+	cout << "====================" << endl;		
 }
