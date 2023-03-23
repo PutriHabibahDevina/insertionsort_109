c46b45c3c1cbe341831f185308898b99f40a00c0
PutriHabibahDevina <putrihdm13@gmail.com>
2023-03-23 19:23:22 +0700

    Membuat kondisi n tidak lebih dari 20 dan menampilkan pesan jika lebih dari 20

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -10,5 +10,14 @@ void input()
 	{
 		cout << "Masukkan Jumlah Data pada Array : ";	// Membuat inputan jumlah element array
 		cin >> n;	// Memanggil variabel inputan n
+
+		if (n <= 20)
+		{	// Membuat kondisi n tidak lebih dari 20
+			break;
+		}
+		else
+		{
+			cout << "\nArray yang anda masukkan maksimal 20 elemen, \n";	// Menampilkan pesan jika data lebih dari 20
+		}
 	}
 }
